3767e4bd2f3293b599dc138c49d0ecd42f12e4a1
partha235 <parthaa235@gmail.com>
2023-10-21 16:18:08 +0530

    updated

--- python_type_practice/morse5.py
+++ python_type_practice/morse5.py
@@ -3,15 +3,17 @@ import string as s
 import random as r
 from time import sleep
 
-while True:
+ch_n=int(input('enter number of character \n '))
+
+for ch in range(ch_n):
     x=r.choice(s.ascii_uppercase)
     y=r.choice(s.digits)
     z=r.choice(s.punctuation)
     char_lis=[x,y,z]
     char_speak=r.choice(char_lis)
-    print(char_speak)
+    print("char_speak = "+char_speak)
     try:
         os.system("espeak "+char_speak)
     except:
-        pass
+        pass 
     sleep(1)
